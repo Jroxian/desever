8f5a590998a7ed1966a15e8f93ce43b58291454c
Jroxian <Jroxian@gmail.com>
2025-03-03 18:48:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "maraam52056@gmail.com";
-//const pass = "ohgvfxvbosxqriug";
-// 23 march
+const pass = "ohgvfxvbosxqriug";
+// 3 april
 // de young
 
 // Middleware
